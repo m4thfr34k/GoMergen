e54bb8933477879f0aeb0676a8bfdd64900cdb40
Daniel Charpentier <daniel.charpentier@gmail.com>
2022-06-08 10:58:50 -0400

    Updated magiceden command logic

--- cmd/magiceden.go
+++ cmd/magiceden.go
@@ -7,11 +7,10 @@ package cmd
 import (
 	"fmt"
 	"github.com/go-resty/resty/v2"
+	"github.com/spf13/cobra"
 	"os"
 	"strconv"
 	"time"
-
-	"github.com/spf13/cobra"
 )
 
 // magicedenCmd represents the magiceden command
@@ -23,18 +22,20 @@ var magicedenCmd = &cobra.Command{
 		meCollection, _ := cmd.Flags().GetString("coll")
 		meBearToken, _ := cmd.Flags().GetString("meapi")
 		rateLimit, _ := cmd.Flags().GetInt("ratelimit")
+		retryLimit, _ := cmd.Flags().GetInt("retry")
+		rpcProviderURL, _ := cmd.Flags().GetString("rpc")
+		if rpcProviderURL == "" {
+			rpcProviderURL = rpcDefault
+		}
 		if meCollection != "" {
 			nftMintAddresses := make(map[string]string)
-			nftMintAddresses, err := getMagicEdenNFTs(meCollection, meBearToken, rateLimit)
+			nftMintAddresses, err := getMagicEdenNFTs(meCollection, meBearToken, rateLimit, retryLimit, rpcProviderURL)
 			if err != nil {
 				fmt.Println("")
 				fmt.Println("Error in attempting to discover NFTs in collection:", err.Error())
 			} else {
 				fmt.Println("")
-				fmt.Println(len(nftMintAddresses))
-				for s, _ := range nftMintAddresses {
-					fmt.Println(s)
-				}
+				fmt.Println(len(nftMintAddresses), " found during this run")
 			}
 
 		} else {
@@ -44,7 +45,7 @@ var magicedenCmd = &cobra.Command{
 	},
 }
 
-func getMagicEdenNFTs(meCollection string, meAuthToken string, rateLimit int) (map[string]string, error) {
+func getMagicEdenNFTs(meCollection string, meAuthToken string, rateLimit int, retryLimit int, rpcProviderURL string) (map[string]string, error) {
 
 	// TODO Refactor this func. It's too long and insane. Working <> good
 	// get unique NFT mint addresses from collection activities
@@ -55,118 +56,130 @@ func getMagicEdenNFTs(meCollection string, meAuthToken string, rateLimit int) (m
 	const magicEdenTokenMintURL string = "https://api-mainnet.magiceden.dev/v2/tokens/"
 
 	nftMintAddresses := make(map[string]string)
-	nftWallet := make(map[string]string)
-	allNFTsFromWallets := make(map[string]string)
 
-	currentOffset := 0
-	returnedRecords := 1
+	for i := 1; i <= (retryLimit + 1); i++ {
 
-	type MEactivity struct {
-		TokenMint string `json:"tokenMint"`
-		Seller    string `json:"seller"`
-	}
+		nftWallet := make(map[string]string)
+		allNFTsFromWallets := make(map[string]string)
 
-	fmt.Print("Working")
+		currentOffset := 0
+		returnedRecords := 1
 
-	// Getting the unique NFT mint addresses from collection activities
-	client := resty.New()
-	for returnedRecords > 0 {
-		var activities []MEactivity
-		_, err := client.R().
-			SetAuthToken(meAuthToken).
-			SetQueryParams(map[string]string{
-				"offset": strconv.Itoa(currentOffset),
-				"limit":  "1000",
-			}).
-			SetResult(&activities).
-			Get(magicEdenCollectionActivitiesURL + meCollection + "/activities")
-		if err != nil {
-			return nftMintAddresses, err
-		} else {
-			returnedRecords = len(activities)
-			for _, activity := range activities {
-				fmt.Print(".")
-				_, ok := nftMintAddresses[activity.TokenMint]
-				if !ok && len(activity.TokenMint) > 0 {
-					nftMintAddresses[activity.TokenMint] = meCollection
-					_ = saveMECollectionNFTs(activity.TokenMint, meCollection)
-				}
-
-				//Getting unique wallets that hold these NFTs so we can check for more from the same collection
-				_, ok = nftWallet[activity.Seller]
-				if !ok && len(activity.TokenMint) > 0 && len(activity.Seller) > 0 {
-					nftWallet[activity.Seller] = "found"
-				}
-			}
+		type MEactivity struct {
+			TokenMint string `json:"tokenMint"`
+			Seller    string `json:"seller"`
+			Buyer     string `json:"buyer"`
 		}
-		currentOffset = currentOffset + 1000
-		if rateLimit > 0 {
-			time.Sleep(time.Duration(1000/rateLimit) * time.Millisecond)
-		}
-	}
 
-	//loop thru wallets and get all nft's stored in the wallets
-	for walletAddress, _ := range nftWallet {
-		currentWalletOffset := 0
-		returnedWalletRecords := 1
-		type WalletContents struct {
-			NFTaddress string `json:"mintAddress"`
-		}
-		for returnedWalletRecords > 0 {
-			var nftsFromWallet []WalletContents
-			_, err := client.R().
+		fmt.Print("Working")
+
+		// Getting the unique NFT mint addresses from collection activities
+		clientResty := resty.New()
+		for returnedRecords > 0 {
+			var activities []MEactivity
+			_, err := clientResty.R().
 				SetAuthToken(meAuthToken).
 				SetQueryParams(map[string]string{
-					"offset":     strconv.Itoa(currentWalletOffset),
-					"limit":      "500",
-					"listStatus": "both",
+					"offset": strconv.Itoa(currentOffset),
+					"limit":  "1000",
 				}).
-				SetResult(&nftsFromWallet).
-				Get(magicEdenWalletContentsURL + walletAddress + "/tokens")
+				SetResult(&activities).
+				Get(magicEdenCollectionActivitiesURL + meCollection + "/activities")
 			if err != nil {
-				// Error in current attempt. Move to next attempt.
+				return nftMintAddresses, err
 			} else {
-				returnedWalletRecords = len(nftsFromWallet)
-				for _, walletData := range nftsFromWallet {
+				returnedRecords = len(activities)
+				for _, activity := range activities {
 					fmt.Print(".")
-					_, ok := allNFTsFromWallets[walletData.NFTaddress]
-					if !ok && len(walletData.NFTaddress) > 0 {
-						allNFTsFromWallets[walletData.NFTaddress] = "found"
+					_, ok := nftMintAddresses[activity.TokenMint]
+					if !ok && len(activity.TokenMint) > 0 {
+						nftMintAddresses[activity.TokenMint] = meCollection
+						_ = saveMECollectionNFTs(activity.TokenMint, meCollection)
+					}
+
+					//Getting unique wallets that hold these NFTs so we can check for more from the same collection
+					_, ok = nftWallet[activity.Seller]
+					if !ok && len(activity.TokenMint) > 0 && len(activity.Seller) > 0 {
+						nftWallet[activity.Seller] = "found"
+					}
+
+					_, ok = nftWallet[activity.Buyer]
+					if !ok && len(activity.TokenMint) > 0 && len(activity.Buyer) > 0 {
+						nftWallet[activity.Buyer] = "found"
 					}
 				}
 			}
-			currentWalletOffset = currentWalletOffset + 500
+			currentOffset = currentOffset + 1000
 			if rateLimit > 0 {
 				time.Sleep(time.Duration(1000/rateLimit) * time.Millisecond)
 			}
 		}
-	}
 
-	//loop thru NFTs from wallets and get all nft's that match the collection we are looking for
-	for singleNFT, _ := range allNFTsFromWallets {
-		type NFTData struct {
-			Collection string `json:"collection"`
-		}
-
-		var nftsData NFTData
-		_, err := client.R().
-			SetAuthToken(meAuthToken).
-			SetResult(&nftsData).
-			Get(magicEdenTokenMintURL + singleNFT)
-		if err != nil {
-			// Error in current attempt. Move to next attempt.
-		} else {
-			fmt.Print(".")
-			if nftsData.Collection == meCollection {
-				_, ok := nftMintAddresses[singleNFT]
-				if !ok && len(singleNFT) > 0 {
-					nftMintAddresses[singleNFT] = "found"
-					_ = saveMECollectionNFTs(singleNFT, meCollection)
+		//loop thru wallets and get all nft's stored in the wallets
+		//this data is coming from Magic Eden
+		for walletAddress, _ := range nftWallet {
+			currentWalletOffset := 0
+			returnedWalletRecords := 1
+			type WalletContents struct {
+				NFTaddress string `json:"mintAddress"`
+			}
+			for returnedWalletRecords > 0 {
+				var nftsFromWallet []WalletContents
+				_, err := clientResty.R().
+					SetAuthToken(meAuthToken).
+					SetQueryParams(map[string]string{
+						"offset":     strconv.Itoa(currentWalletOffset),
+						"limit":      "500",
+						"listStatus": "both",
+					}).
+					SetResult(&nftsFromWallet).
+					Get(magicEdenWalletContentsURL + walletAddress + "/tokens")
+				if err != nil {
+					// Error in current attempt. Move to next attempt.
+				} else {
+					returnedWalletRecords = len(nftsFromWallet)
+					for _, walletData := range nftsFromWallet {
+						fmt.Print(".")
+						_, ok := allNFTsFromWallets[walletData.NFTaddress]
+						if !ok && len(walletData.NFTaddress) > 0 {
+							allNFTsFromWallets[walletData.NFTaddress] = "found"
+						}
+					}
+				}
+				currentWalletOffset = currentWalletOffset + 500
+				if rateLimit > 0 {
+					time.Sleep(time.Duration(1000/rateLimit) * time.Millisecond)
 				}
 			}
 		}
-		if rateLimit > 0 {
-			time.Sleep(time.Duration(1000/rateLimit) * time.Millisecond)
+
+		//loop thru NFTs from wallets and get all nft's that match the collection we are looking for
+		for singleNFT, _ := range allNFTsFromWallets {
+			type NFTData struct {
+				Collection      string `json:"collection"`
+				UpdateAuthority string `json:"updateAuthority"`
+			}
+
+			var nftsData NFTData
+			_, err := clientResty.R().
+				SetAuthToken(meAuthToken).
+				SetResult(&nftsData).
+				Get(magicEdenTokenMintURL + singleNFT)
+			if err != nil {
+				// Error in current attempt. Move to next attempt.
+			} else {
+				fmt.Print(".")
+				if nftsData.Collection == meCollection {
+					_, ok := nftMintAddresses[singleNFT]
+					if !ok && len(singleNFT) > 0 {
+						nftMintAddresses[singleNFT] = "found"
+						_ = saveMECollectionNFTs(singleNFT, meCollection)
+					}
+				}
+			}
+			if rateLimit > 0 {
+				time.Sleep(time.Duration(1000/rateLimit) * time.Millisecond)
+			}
 		}
 	}
 
